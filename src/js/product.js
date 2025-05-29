63aa07ae81dce70367b489c41ee635deb3275e0a
gabrieldubiela <gabriel.dubiela@hotmail.com>
2025-05-28 22:21:49 -0300

    fix product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,20 @@
-import {getParam} from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
-const product = new ProductDetails(productId, dataSource);
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
 product.init();
 
-console.log(dataSource.findProductById(productId));
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
