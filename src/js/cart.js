280a0e42e5576799fd79003ecf1f8d863061b436
gabrieldubiela <gabriel.dubiela@hotmail.com>
2025-05-31 18:35:48 -0300

    trying to fix the cart... now stop open the products

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,4 @@
-import {
-  getLocalStorage,
-  setLocalStorage,
-  loadHeaderFooter,
-} from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
@@ -10,14 +6,15 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  // New function to remove an item from the cart
   const excludeButton = document.querySelectorAll(".cart-card__button");
 
-  excludeButton.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const itemId = event.currentTarget.dataset.itemId;
-      removeItem(itemId);
+  excludeButton.forEach(button => {
+      button.addEventListener("click", (event) => {
+          const itemId = event.currentTarget.dataset.itemId;
+          removeItem(itemId);
     });
-  });
+});
 }
 
 function cartItemTemplate(item) {
@@ -34,7 +31,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button class="cart-card__button" data-item-id="${item.Id}"><img src="../images/red_trash_bin.webp" alt="bin"></button>
+  <button class="cart-card__button" data-item-id="${item.Id}"><img src="../public/images/red_trash_bin.webp" alt="bin"></button>
 </li>`;
 
   return newItem;
@@ -44,7 +41,7 @@ renderCartContents();
 
 function removeItem(itemId) {
   let cartItems = getLocalStorage("so-cart") || [];
-  let index = cartItems.findIndex((item) => item.Id === itemId);
+  let index = cartItems.findIndex(item => item.Id === itemId);
   cartItems.splice(index, 1);
   setLocalStorage("so-cart", cartItems);
   renderCartContents();
